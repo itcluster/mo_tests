da0f55961aa305b00bd48c8015b3586e109ef1f2
Pecho <pecho@2ba.su>
2019-03-10 19:28:33 +0300

    edit comments in mo_tree_visuzal; test it; start work on xgboost_test

--- mo_tree_visuzal.py
+++ mo_tree_visuzal.py
@@ -8,16 +8,14 @@ from sklearn.datasets import load_breast_cancer
 from sklearn import tree
 from sklearn.tree import export_graphviz
 
-# Здесь мы инициализируем и тренируем модель дерева решений
+#init decision tree model and train it
 cancer = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(
 cancer.data, cancer.target, stratify=cancer.target, random_state=42)
 clf = tree.DecisionTreeClassifier(max_depth=4, random_state=0)
 clf = clf.fit(X_train, y_train)
 
-# Здесь экспортируется дерево вроде, однако мы все данные записываем в дот_дата, как я понял
-# Потом мы получем граф из этой дот_даты и записываем в пдф
-
+# create graph from dot data
 import graphviz
 import pydotplus
 dot_data = tree.export_graphviz(clf, out_file=None)

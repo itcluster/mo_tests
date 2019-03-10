bba5ec5b0ac91180bdaf31bfc74569f8dc5b5059
AlexanderOfMacedon <tam2511@mail.ru>
2019-03-10 21:33:45 +0300

    add extra.py

---
+++ extra.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def plot_feature_importances_cancer(model, data_):
+    n_features = data_.data.shape[1]
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), data_.feature_names)
+    plt.xlabel("Важность признака")
+    plt.show()
